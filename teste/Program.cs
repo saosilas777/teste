47bf3507647333c4c974a98744a390748205c6cb
Silas Dias <saosilas@hotmail.com>
2021-08-24 21:00:03 -0300

    hello word

---
+++ teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("shit mother foca");
+        }
+    }
+}
